e21c9aca8de4c9484d82e88e22f57850c62ba3a3
Ayumi Ono <ayumiono.1223@gmail.com>
2019-01-22 18:05:05 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello form index.js')
+// 'Hello form index.js'
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXl1bWlvIiwiYSI6ImNqcjhjOWswNzA1YXczeXM3dzkyZzV5ZWMifQ.dfBcioN2OLShCplu8WhH6w';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
